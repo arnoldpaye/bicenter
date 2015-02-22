fe30d7eb59b63ef0e2e3d1b4f55a2df72c945739
Arnold Paye <arnoldpayet@gmail.com>
2015-02-22 09:09:46 -0400

    initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bicenterApp
+ * @description
+ * # bicenterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bicenterApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bicenterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bicenterApp
+ */
+angular.module('bicenterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
